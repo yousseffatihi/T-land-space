a047d297818e36615eb1480a1e42cfaa8b8c9df3
Youssef Fatihi <Usefps@gmail.com>
2019-05-20 13:34:12 +0000

    Add files via upload

--- WorkSpace-Web/controllers/promotionController.php
+++ WorkSpace-Web/controllers/promotionController.php
@@ -3,18 +3,54 @@ require_once('../models/config.php');
 require_once('../models/Promotion.php');
 
 class promotionController{
+  /* FUNCTION CAN RETURN NEW ID GENERATED FROM BDD */
   public function getNewId(){
-	  $res = execQuery("SELECT max(IdPromotion) from Promotion");
+	  $res = execQuery("SELECT max(idPromotion) from Promotion");
 	  return mysqli_fetch_field($res);
   }
+  
+  /* FUNCTION CAN GET ALL PROMOTION FROM BDD THEN HE RETURN ARRAY OF ADMIN */
   public function getAll(){
-    return execQuery("SELECT * from Promotion");
+    $res = execQuery("SELECT * from Promotion");
+    $promos = array();
+	while ($data = mysqli_fetch_assoc($res)){
+        $promos[] = new Promotion($data['idPromotion'],
+		                          $data['NameEvent'],
+		                          $data['TextEvent'],
+		                          $data['Image'],
+		                          $data['DatePost'],
+		                          $data['File'],
+								  $data['DatePromotion'],
+		                          $data['DateExpiration'],
+		                          $data['idAdministrator']);
+	}
+	
+	return $promos;
   }
+  
+  /* FUNCTION CAN GET PROMOTION BY HIS ID FROM BDD THEN HE RETURN ADMIN */
   public function getById($id){
-    return execQuery("SELECT * from Promotion Where IdPromotion=".$id);
+	
+	/* TEST IF 'ID' IS NUMERIC */
+    if(!is_numeric($id)) return null;
+	  
+    $res = execQuery("SELECT * from Promotion Where idPromotion=".$id);
+	
+	/* TEST IF TABLE EXIST */
+	if(!$res) return null;
+	
+	$data = mysqli_fetch_row($res);
+	$promo = new Promotion($data[0],$data[1],$data[2],$data[3],$data[4],$data[5],$data[6],$data[7],$data[8]);
+    return $promo;
   }
+  
+  /* FUNCTION CAN INSERT NEW PROMOTION IN BDD */
+  /* -1 : argument is not object */
+  /*  1 : data inserted succefully */
+  /*  0 : data not inserted */
   public function add($promo){
 	  if(!is_object($promo)) return -1;
+	  
 	  $req = "INSERT INTO Promotion(NamePromotion,TextPromotion,Image,DatePost,File,DatePromotion,DateExpiration,idAdministrator) VALUES('"
 	  .$promo->getTitle().
 	  "','"
@@ -33,18 +69,24 @@ class promotionController{
 	  .$ownerId.
 	  "')";
 	  
-	  $req = execQuery($req);
-	  if($req)
+	  $res = execQuery($req);
+	  if($res)
 		  return 1;
 	  else
 		  return 0;
   }
   
+  /* FUNCTION CAN DELETE PROMOTION IN BDD */
+  /* -1 : argument is not numeric */
+  /*  1 : data deleted succefully */
+  /*  0 : data not deleted */
   public function del($id){
 	  if(!is_numeric($id)) return -1;
-	  $req = "DELETE from Promotion WHERE IdPromotion=".$id;
-	  $req = execQuery($req);
-	  if($req)
+	  
+	  $req = "DELETE from Promotion WHERE idPromotion=".$id;
+	  $res = execQuery($req);
+	  
+	  if($res)
 		  return 1;
 	  else
 		  return 0;
--- WorkSpace-Web/controllers/subscriptionController.php
+++ WorkSpace-Web/controllers/subscriptionController.php
@@ -3,15 +3,41 @@ require_once('../models/config.php');
 require_once('../models/Subscription.php');
 
 class subscriptionController{
+  /* FUNCTION CAN GET ALL SUBSCRIPTION FROM BDD THEN HE RETURN ARRAY OF ADMIN */
   public function getAll(){
-    return execQuery("SELECT * from Subscription");
+    $res = execQuery("SELECT * from Subscription");
+    $subs = array();
+	while ($data = mysqli_fetch_assoc($res)){
+        $subs[] = new Subscription($data['idClient'],$data['idEvent'],$data['DateSubscription']);
+	}
+	
+	return $subs;
   }
+  
+  /* FUNCTION CAN GET SUBSCRIPTION BY HIS ID FROM BDD THEN HE RETURN ADMIN */
   public function getById($id){
-    return execQuery("SELECT * from Subscription Where IdEvent=".$id);
+	
+	/* TEST IF 'ID' IS NUMERIC */
+    if(!is_numeric($id)) return null;
+	  
+    $res = execQuery("SELECT * from Subscription Where idEvent=".$id);
+	
+	/* TEST IF TABLE EXIST */
+	if(!$res) return null;
+	
+	$data = mysqli_fetch_row($res);
+	$sub = new Subscription($data[0],$data[1],$data[2]);
+    return $sub;
   }
+  
+  /* FUNCTION CAN INSERT NEW SUBSCRIPTION IN BDD */
+  /* -1 : argument is not object */
+  /*  1 : data inserted succefully */
+  /*  0 : data not inserted */
   public function add($sub){
 	  if(!is_object($sub)) return -1;
-	  $req = "INSERT INTO Subscription(IdClient,IdEvent,DateSubscription) VALUES('"
+	  
+	  $req = "INSERT INTO Subscription(idClient,idEvent,DateSubscription) VALUES('"
 	  .$sub->getClient()->getIdPerson();
 	  "','"
 	  .$sub->getEvent()->getIdPost().
@@ -19,18 +45,24 @@ class subscriptionController{
 	  .$sub->getDateSubscription().
 	  "')";
 	  
-	  $req = execQuery($req);
-	  if($req)
+	  $res = execQuery($req);
+	  if($res)
 		  return 1;
 	  else
 		  return 0;
   }
   
+  /* FUNCTION CAN DELETE SUBSCRIPTION IN BDD */
+  /* -1 : argument is not numeric */
+  /*  1 : data deleted succefully */
+  /*  0 : data not deleted */
   public function del($id){
 	  if(!is_numeric($id)) return -1;
-	  $req = "DELETE from Subscription WHERE IdEvent=".$id;
-	  $req = execQuery($req);
-	  if($req)
+	  
+	  $req = "DELETE from Subscription WHERE idEvent=".$id;
+	  $res = execQuery($req);
+	  
+	  if($res)
 		  return 1;
 	  else
 		  return 0;
