06d0ff48558f188bb3fe3ecd404cfaa5644abed2
Youssef Fatihi <Usefps@gmail.com>
2019-05-22 19:02:13 +0200

    Timeline

---
+++ WorkSpace-Web/views/Timeline.php
@@ -0,0 +1,185 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="../libs/css/font-awesome.min.css" type="text/css">
+  <link rel="stylesheet" href="../libs/css/theme.css" type="text/css">
+  <link rel="stylesheet" href="../libs/css/custom.css" type="text/css">
+  <link href="../libs/css/datepicker.min.css" rel="stylesheet" type="text/css">
+  <style media="screen">
+  .recentPost{
+    white-space: normal;
+    height: 70px;
+    overflow: hidden;
+    text-overflow: ellipsis;
+  }
+
+  .datePost{
+    align-items: baseline;
+  }
+
+  .postTitle{
+    white-space: nowrap;
+    width: 100%;
+    overflow: hidden;
+    text-overflow: ellipsis;
+  }
+
+  .postContent{
+
+  }
+
+  .list-group-item{
+    border: none;
+  }
+  </style>
+</head>
+
+<body>
+  <!-- NavBar -->
+  <nav class="navbar navbar-expand-md navbar-light">
+    <div class="container"> <a class="navbar-brand text-primary" href="#">
+        <i class="fa d-inline fa-lg fa-stop-circle"></i>
+        <b> BRAND</b>
+      </a> <button class="navbar-toggler navbar-toggler-right border-0" type="button" data-toggle="collapse" data-target="#navbar4">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbar4">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item"> <a class="nav-link" href="#">Home</a> </li>
+          <li class="nav-item"> <a class="nav-link" href="#">My Reservation</a> </li>
+          <li class="nav-item"> <a class="nav-link" href="#">Events</a> </li>
+          <li class="nav-item"> <a class="nav-link" href="#">Profile</a> </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+  <!-- End Navbar -->
+  <!-- TimeLine -->
+  <div class="py-5">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-8">
+          <div class="card mb-4">
+            <img class="card-img-top" src="https://static.pingendo.com/cover-moon.svg" alt="Card image cap">
+            <div class="card-body">
+              <small>by <strong>Admin</strong> On May 12, 2017</small>
+              <h4 class="card-title mt-1">Card title</h4>
+              <p class="card-text postTitle">Some quick example text to build on the card title and make up the bulk of the card's content. safasdfsae sadfsakdfhjaskjdfhkasjdhfklajhsdfk ajksfghlkasjdfhlkjh</p> <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+          </div>
+          <div class="card mb-4">
+            <img class="card-img-top" src="https://static.pingendo.com/cover-moon.svg" alt="Card image cap">
+            <div class="card-body">
+              <small>by <strong>Admin</strong> On May 12, 2017</small>
+              <h4 class="card-title mt-1">Card title</h4>
+              <p class="card-text postTitle">Some quick example text to build on the card title and make up the bulk of the card's content.</p> <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+          </div>
+          <div class="card mb-4">
+            <img class="card-img-top" src="https://static.pingendo.com/cover-moon.svg" alt="Card image cap">
+            <div class="card-body">
+              <small>by <strong>Admin</strong> On May 12, 2017</small>
+              <h4 class="card-title mt-1">Card title</h4>
+              <p class="card-text postTitle">Some quick example text to build on the card title and make up the bulk of the card's content.</p> <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="card-body">
+            <h5 class="card-title"><b>Categories</b></h5>
+            <ul class="list-group">
+              <li class="list-group-item d-flex justify-content-between align-items-center"> Cras justo odio <span class="badge badge-primary badge-pill">14</span> </li>
+              <li class="list-group-item d-flex justify-content-between align-items-center"> Dapibus ac facilisis in <span class="badge badge-primary badge-pill">2</span> </li>
+              <li class="list-group-item d-flex justify-content-between align-items-center"> Morbi leo risus <span class="badge badge-primary badge-pill">1</span> </li>
+            </ul>
+          </div>
+          <div class="card-body">
+            <h5 class="card-title"><b>Recent Posts</b><br></h5>
+            <div class="list-group">
+              <a href="#" class="list-group-item list-group-item-action flex-column align-items-start">
+                <div class="d-flex w-100 justify-content-between datePost">
+                  <h5 class="mb-2 postTitle">Top 10 Things You Can Do In Summer!</h5>
+                </div>
+                <div class="row">
+                  <div class="col-md-4"><img class="img-fluid d-block" src="https://static.pingendo.com/img-placeholder-1.svg"></div>
+                  <div class="col-md-8">
+                    <p class="recentPost">Paragraph. Then, my friend, when darkness overspreads my eyes, and heaven and earth seem to dwell in my soul and absorb its power, like the form of a beloved mistress, then I often think with longing.</p>
+                  </div>
+                </div>
+              </a><a href="#" class="list-group-item list-group-item-action flex-column align-items-start">
+                <div class="d-flex w-100 justify-content-between datePost">
+                  <h5 class="mb-2 postTitle">Let's Read Some Books Together!</h5>
+                </div>
+                <div class="row">
+                  <div class="col-md-4"><img class="img-fluid d-block" src="https://static.pingendo.com/img-placeholder-1.svg"></div>
+                  <div class="col-md-8">
+                    <p class="recentPost">Paragraph. Then, my friend, when darkness overspreads my eyes, and heaven and earth seem to dwell in my soul and absorb its power, like the form of a beloved mistress, then I often think with longing.</p>
+                  </div>
+                </div>
+              </a><a href="#" class="list-group-item list-group-item-action flex-column align-items-start">
+                <div class="d-flex w-100 justify-content-between datePost">
+                  <h5 class="mb-2 postTitle">Let's Watch Movies Together!</h5>
+                </div>
+                <div class="row">
+                  <div class="col-md-4"><img class="img-fluid d-block" src="https://static.pingendo.com/img-placeholder-1.svg"></div>
+                  <div class="col-md-8">
+                    <p class="recentPost">Paragraph. Then, my friend, when darkness overspreads my eyes, and heaven and earth seem to dwell in my soul and absorb its power, like the form of a beloved mistress, then I often think with longing.</p>
+                  </div>
+                </div>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- TimeLine -->
+  <!-- footer -->
+  <div class="">
+    <div class="container">
+      <div class="row">
+        <div class="p-4 col-md-4">
+          <h2 class="mb-4">T Land Space</h2>
+          <p>A company for whatever you may need, from website prototyping to publishing</p>
+        </div>
+        <div class="p-4 col-md-4">
+          <h2 class="mb-4">Mapsite</h2>
+          <ul class="list-unstyled">
+            <a href="#" class="text-dark">Home</a>
+            <br>
+            <a href="#" class="text-dark">About us</a>
+            <br>
+            <a href="#" class="text-dark">Our services</a>
+             <br>
+             <a href="#" class="text-dark">Stories</a>
+           </ul>
+        </div>
+        <div class="p-4 col-md-4">
+          <h2 class="mb-4">Contact</h2>
+          <p> <a href="#" class="text-dark">
+              <i class="fa d-inline mr-3 text-muted fa-phone"></i>+246 - 542 550 5462</a> </p>
+          <p> <a href="#" class="text-dark">
+              <i class="fa d-inline mr-3 text-muted fa-envelope-o"></i>info@pingendo.com</a> </p>
+          <p> <a href="#" class="text-dark">
+              <i class="fa d-inline mr-3 fa-map-marker text-muted"></i>365 Park Street, NY</a> </p>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-md-12 mt-3">
+          <p class="text-center">© Copyright 2019 T Land Space - All rights reserved. </p>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- End footer -->
+  <script src="../libs/js/jquery.min.js"></script>
+  <script src="../libs/js/popper.min.js"></script>
+  <script src="../libs/js/bootstrap.min.js"></script>
+  <link href="../libs/css/datepicker.min.css" rel="stylesheet" type="text/css">
+  <script src="../libs/js/datepicker.min.js"></script>
+  <script src="../libs/js/i18n/datepicker.en.js"></script>
+</body>
+
+</html>
