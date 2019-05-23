7d4e73545786b275ab0a82fb33ee3e92cc489f12
Youssef Fatihi <Usefps@gmail.com>
2019-05-23 20:42:51 +0200

    Create Events.php

---
+++ WorkSpace-Web/views/Events.php
@@ -0,0 +1,181 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="../libs/css/all.css" type="text/css">
+  <link rel="stylesheet" href="../libs/css/theme.css" type="text/css">
+  <link rel="stylesheet" href="../libs/css/custom.css" type="text/css">
+  <link href="../libs/css/datepicker.min.css" rel="stylesheet" type="text/css">
+  <style media="screen">
+  .recentPost{
+    white-space: normal;
+    height: 70px;
+    overflow: hidden;
+    text-overflow: ellipsis;
+  }
+
+  .datePost{
+    align-items: baseline;
+  }
+
+  .postTitle{
+    white-space: nowrap;
+    width: 100%;
+    overflow: hidden;
+    text-overflow: ellipsis;
+  }
+
+  .postContent{
+
+  }
+
+  .imgComment{
+    max-width: 96px;
+  }
+
+  .txtComment{
+    width: 100% !important;
+  }
+
+  .btnComment{
+        margin-left: auto;
+  }
+
+  .list-group-item{
+    border: none;
+  }
+
+  hr{
+        border-top: 1px dashed rgba(0, 0, 0, 0.1);
+  }
+
+  a{
+    text-decoration: none !important;
+  }
+
+  .btnPrice{
+    margin: auto;
+width: 100%;
+position: absolute;
+top: 0;
+left: 0;
+  }
+  </style>
+</head>
+
+<body>
+  <!-- NavBar -->
+  <nav class="navbar navbar-expand-md navbar-light">
+    <div class="container"> <a class="navbar-brand text-primary" href="#">
+        <i class="fa d-inline fa-lg fa-stop-circle"></i>
+        <b> BRAND</b>
+      </a> <button class="navbar-toggler navbar-toggler-right border-0" type="button" data-toggle="collapse" data-target="#navbar4">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbar4">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item"> <a class="nav-link" href="#">Home</a> </li>
+          <li class="nav-item"> <a class="nav-link" href="#">My Reservation</a> </li>
+          <li class="nav-item"> <a class="nav-link" href="#">Events</a> </li>
+          <li class="nav-item"> <a class="nav-link" href="#">Profile</a> </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+  <!-- End Navbar -->
+  <div class="py-5">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-3">
+            <ul class="list-group">
+              <li class=" border-0 list-group-item d-flex justify-content-between align-items-center">
+                <a href="#"><i class="fas fa-user-circle fa-lg"></i> <span class="ml-2">Profile</span> </a>
+              </li>
+              <li class=" border-0 list-group-item d-flex justify-content-between align-items-center">
+                <a href="#"><i class="fas fa-lock fa-lg"></i> <span class="ml-2">Password</span> </a>
+              </li>
+              <li class=" border-0 list-group-item d-flex justify-content-between align-items-center">
+                <a href="#"><i class="fas fa-handshake fa-lg"></i> <span class="ml-2">Events</span> </a>
+              </li>
+              <li class=" border-0 list-group-item d-flex justify-content-between align-items-center">
+                <a href="#"><i class="fas fa-calendar fa-lg"></i> <span class="ml-2">My Reservations</span> </a>
+              </li>
+            </ul>
+          </div>
+          <div class="col-md-9">
+            <div class="card mb-4">
+              <img class="card-img-top" src="https://static.pingendo.com/cover-moon.svg" alt="Card image cap">
+              <div class="card-body">
+                <small>by <strong>Admin</strong> On May 12, 2017</small>
+                <h4 class="card-title mt-1">Card title</h4>
+                <p class="card-text postTitle">Some quick example text to build on the card title and make up the bulk of the card's content. safasdfsae sadfsakdfhjaskjdfhkasjdhfklajhsdfk ajksfghlkasjdfhlkjh</p> <a href="#" class="btn btn-primary">Go somewhere</a>
+              </div>
+            </div>
+            <div class="card mb-4">
+              <img class="card-img-top" src="https://static.pingendo.com/cover-moon.svg" alt="Card image cap">
+              <div class="card-body">
+                <small>by <strong>Admin</strong> On May 12, 2017</small>
+                <h4 class="card-title mt-1">Card title</h4>
+                <p class="card-text postTitle">Some quick example text to build on the card title and make up the bulk of the card's content.</p> <a href="#" class="btn btn-primary">Go somewhere</a>
+              </div>
+            </div>
+            <div class="card mb-4">
+              <img class="card-img-top" src="https://static.pingendo.com/cover-moon.svg" alt="Card image cap">
+              <div class="card-body">
+                <small>by <strong>Admin</strong> On May 12, 2017</small>
+                <h4 class="card-title mt-1">Card title</h4>
+                <p class="card-text postTitle">Some quick example text to build on the card title and make up the bulk of the card's content.</p> <a href="#" class="btn btn-primary">Go somewhere</a>
+              </div>
+            </div>
+        </div>
+        </div>
+      </div>
+    </div>
+  <!-- footer -->
+  <div class="">
+    <div class="container">
+      <div class="row">
+        <div class="p-4 col-md-4">
+          <h2 class="mb-4">T Land Space</h2>
+          <p>A company for whatever you may need, from website prototyping to publishing</p>
+        </div>
+        <div class="p-4 col-md-4">
+          <h2 class="mb-4">Mapsite</h2>
+          <ul class="list-unstyled">
+            <a href="#" class="text-dark">Home</a>
+            <br>
+            <a href="#" class="text-dark">About us</a>
+            <br>
+            <a href="#" class="text-dark">Our services</a>
+             <br>
+             <a href="#" class="text-dark">Stories</a>
+           </ul>
+        </div>
+        <div class="p-4 col-md-4">
+          <h2 class="mb-4">Contact</h2>
+          <p> <a href="#" class="text-dark">
+              <i class="fa d-inline mr-3 text-muted fa-phone"></i>+246 - 542 550 5462</a> </p>
+          <p> <a href="#" class="text-dark">
+              <i class="fa d-inline mr-3 text-muted fa-envelope-o"></i>info@pingendo.com</a> </p>
+          <p> <a href="#" class="text-dark">
+              <i class="fa d-inline mr-3 fa-map-marker text-muted"></i>365 Park Street, NY</a> </p>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-md-12 mt-3">
+          <p class="text-center">© Copyright 2019 T Land Space - All rights reserved. </p>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- End footer -->
+  <script src="../libs/js/jquery.min.js"></script>
+  <script src="../libs/js/popper.min.js"></script>
+  <script src="../libs/js/bootstrap.min.js"></script>
+  <link href="../libs/css/datepicker.min.css" rel="stylesheet" type="text/css">
+  <script src="../libs/js/datepicker.min.js"></script>
+  <script src="../libs/js/i18n/datepicker.en.js"></script>
+</body>
+
+</html>
