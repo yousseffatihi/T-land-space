74d4126b7e97d77265eea7ca91c931db5934e54a
Youssef Fatihi <Usefps@gmail.com>
2019-06-01 12:20:18 +0000

    Add files via upload

--- WorkSpace-Web/views/Post.php
+++ WorkSpace-Web/views/Post.php
@@ -1,9 +1,15 @@
+<?php 
+require_once('../controllers/postController.php');
+require_once('../controllers/administratorController.php');
+require_once('../controllers/promotionController.php');
+require_once('../controllers/eventController.php');
+?>
 <!DOCTYPE html>
 <html>
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>T Land Space | <?php echo $_GET['post']; ?></title>
+  <title>T Land Space | POST</title>
   <link rel="stylesheet" href="../libs/css/all.css" type="text/css">
   <link rel="stylesheet" href="../libs/css/theme.css" type="text/css">
   <link rel="stylesheet" href="../libs/css/custom.css" type="text/css">
@@ -34,9 +40,20 @@
   <div class="py-3">
     <div class="container">
       <div class="row">
+	    <?php
+		    $postId = $_GET['postId'];
+			$pc = new postController();
+			$ec = new eventController();
+			$promo = new promotionController();
+			$post = $pc->getById($postId);
+			$ac = new administratorController();
+			$admin = $ac->getById($post->getAdmin());
+			$date = $post->getDatePost();
+			$d = new DateTime($date); 
+		?>
         <div class="col-md-12 text-center">
-          <h1 class="">Let's Read Some Books Together And Share Common Ideas</h1>
-          <h5 class="">Posted by Admin on May 12, 2017</h5>
+          <h1 class=""><?php echo $post->getTitle(); ?></h1>
+          <h5 class="">Posted by Admin on <?php echo $d->format('M').' '.$d->format('d').', '.$d->format('Y') ?></h5>
         </div>
       </div>
     </div>
@@ -44,7 +61,10 @@
   <div class="py-5" style="	background-image: url(&quot;https://pingendo.com/site-assets/cover.jpg&quot;);	background-position: center;	background-size: 100%;	background-repeat: repeat;	min-height: 200px;	height: 600px;">
     <div class="container">
       <div class="row">
-        <div class="col-md-12"></div>
+	    <?php
+		  $img = "../libs/imguploaded/".$post->getImage();
+		?>
+        <div class="col-md-12"><img src=<?php echo $img; ?> width=920px></div>
       </div>
     </div>
   </div>
@@ -54,7 +74,8 @@
         <div class="col-md-8">
             <div class="card text-center postContent mb-3">
               <div class="card-body text-left">
-                <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam et eros enim. Etiam luctus luctus viverra. Nunc in vestibulum ipsum. Nam porttitor auctor elit. Aenean sodales arcu lorem, sed iaculis quam pharetra id. Duis tincidunt massa a risus mattis, ut aliquam ante porttitor. Maecenas consequat libero nunc, et porttitor metus fringilla at. Nunc sed dapibus turpis, eget fermentum nibh. Pellentesque vel volutpat dui. Nam eleifend, ipsum eu sodales efficitur, felis erat tempus neque, ornare sagittis sem leo sit amet ipsum. Sed tincidunt nisi nec congue aliquam. Pellentesque tempor sed dui sit amet volutpat. Pellentesque dictum felis id ante posuere dapibus. Cras sed enim orci.<br><br>Aliquam pharetra, orci quis facilisis mollis, eros turpis aliquet dui, quis tristique massa nulla vitae odio. Sed dolor massa, ultricies nec ornare ac, gravida sit amet augue. Pellentesque lectus lacus, placerat quis enim a, vestibulum ullamcorper est. Sed vitae arcu sed nunc malesuada tincidunt. Praesent ac quam vel odio porttitor convallis. Morbi suscipit nisi tempus nisl iaculis, a convallis diam dictum. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.<br><br>Nullam ullamcorper lacinia sollicitudin. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi dictum viverra quam eget sodales. Maecenas sollicitudin, erat et suscipit rutrum, justo turpis pretium ante, sed ullamcorper lacus sapien id enim. Donec at consectetur mauris. Vestibulum vitae ante ligula. Fusce luctus laoreet ipsum, quis interdum lacus dictum sit amet.<br><br>Mauris nulla enim, rutrum et vestibulum nec, vestibulum eu mi. Sed aliquet, lectus nec tempor aliquet, tortor eros lobortis nisi, id ultricies mi massa vitae metus. Mauris ut turpis malesuada, tincidunt nulla in, molestie dui. Cras feugiat, nulla a accumsan lacinia, ligula justo cursus ligula, vitae fermentum tellus urna a turpis. Aenean eget faucibus turpis. Integer ac pharetra nunc. Nam sollicitudin vestibulum dui vehicula dictum.&nbsp;<br><br>Curabitur eu sem ante. Ut ipsum dolor, porta nec tortor sed, venenatis elementum velit. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec in eleifend lorem. Etiam eu felis ac lacus pellentesque rhoncus. Phasellus nec viverra diam. Morbi dui arcu, commodo eget suscipit quis, mollis non diam. Nullam gravida porttitor libero. Aenean commodo lorem nec mi fermentum, sit amet ornare neque ultricies. Donec suscipit sem ac nulla semper, fringilla viverra erat fringilla. Nunc eget nisl vel nisi semper hendrerit nec quis risus.&nbsp;<br><br>Sed eu auctor magna. Nulla laoreet leo sit amet mi pharetra, in tempus nisl mollis. Etiam ac libero quis eros interdum rutrum. Proin ultricies metus at lorem elementum posuere. Nam scelerisque leo eu suscipit placerat. Integer fermentum auctor turpis non consequat. Quisque euismod, urna in viverra tincidunt, quam arcu aliquet tellus, in egestas nulla ante a augue. Ut sollicitudin sit amet leo ac faucibus. Nam eget lacus commodo, pretium felis in, cursus sem. Nunc viverra dui libero, vel hendrerit nisi dapibus ac.&nbsp;<br><br>Morbi eu interdum nisi, luctus dictum nisl. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Sed in est massa. Nam tempor, quam sit amet interdum tincidunt, orci turpis fringilla nulla, ut iaculis velit tortor id dui. Vivamus vitae lectus at felis volutpat pretium in ornare diam. Morbi quis mi nec ipsum ultrices lacinia. Etiam lectus nulla, mattis a lectus sit amet, mattis malesuada neque.&nbsp;<br><br>Integer vitae turpis quis tellus elementum vulputate. Quisque a ante nisi. Donec hendrerit tincidunt dolor, eu pulvinar sapien blandit eget. Vestibulum sed finibus diam, et congue sapien. Suspendisse elementum auctor mattis. Sed mi sem, ultrices a lacus eget, vulputate sodales nisl. Integer sed elementum velit, in porttitor felis. Vestibulum eget risus sed est consectetur elementum. Sed lacus leo, malesuada ut nibh at, mollis tincidunt urna.&nbsp;<br><br>Vestibulum sit amet est et dui placerat auctor sed sit amet risus. Duis fermentum ex tortor, ut lobortis est accumsan quis. Aliquam efficitur ligula non urna accumsan eleifend. Etiam posuere tortor in metus sodales sagittis. Mauris sodales massa sed pellentesque efficitur. Ut fermentum lectus sapien, a vehicula ante pretium eu. Curabitur a imperdiet diam. Pellentesque eros libero, vestibulum quis eros eget, viverra feugiat massa. Nunc purus purus, tincidunt accumsan felis sed, congue dapibus eros.&nbsp;<br><br>&nbsp;Fusce nec lobortis quam. Nam quis turpis tincidunt, maximus mi sit amet, sollicitudin ex. Duis elit leo, porta semper felis sit amet, suscipit tincidunt augue. Phasellus vestibulum tortor ipsum, ut aliquet mauris fermentum et. Etiam vitae dictum arcu, nec pretium lacus. Integer commodo auctor facilisis. Suspendisse laoreet dapibus ipsum, nec sagittis eros molestie a. Sed vitae est et nisi euismod facilisis. Aliquam finibus finibus nisi lobortis facilisis. Pellentesque venenatis, lacus vitae pharetra lobortis, nunc dolor commodo lectus, dapibus semper lacus tortor at sem. Duis auctor, urna id ornare feugiat, tortor nisl egestas lorem, eu convallis elit libero eu mauris. Pellentesque porta turpis eget imperdiet congue.
+                <p class="card-text">
+				  <?php echo $post->getText(); ?>
                 </p>
                 <hr>
                 <a class="btn btn-primary" style="float: right;" href="#">Apply</a>
@@ -80,12 +101,19 @@
             </div>
         </div>
         <div class="col-md-4">
+		    <?php 
+			$post_count = sizeof($pc->getAll());
+			$promo_count = sizeof($promo->getAll());;
+			$event_count = sizeof($ec->getAll());
+			
+			
+			?>
             <div class="card-body">
               <h5 class="card-title"><b>Categories</b></h5>
               <ul class="list-group">
-                <li class="list-group-item d-flex justify-content-between align-items-center"> Cras justo odio <span class="badge badge-primary badge-pill">14</span> </li>
-                <li class="list-group-item d-flex justify-content-between align-items-center"> Dapibus ac facilisis in <span class="badge badge-primary badge-pill">2</span> </li>
-                <li class="list-group-item d-flex justify-content-between align-items-center"> Morbi leo risus <span class="badge badge-primary badge-pill">1</span> </li>
+                <li class="list-group-item d-flex justify-content-between align-items-center"> Posts <span class="badge badge-primary badge-pill"><?php echo $post_count; ?></span> </li>
+                <li class="list-group-item d-flex justify-content-between align-items-center"> Promotions <span class="badge badge-primary badge-pill"><?php echo $promo_count; ?></span> </li>
+                <li class="list-group-item d-flex justify-content-between align-items-center"> Events <span class="badge badge-primary badge-pill"><?php echo $event_count; ?></span> </li>
               </ul>
             </div>
             <div class="card-body">
@@ -97,44 +125,35 @@
               </div>
               <div class="row">
                 <div class="col-md-12">
-                  <h1 class="text-center py-2">Jeff Bezose<br></h1>
+                  <h1 class="text-center py-2"><?php echo $admin->getLastName().' '.$admin->getFirstName();	?><br></h1>
                 </div>
               </div>
             </div>
             <div class="card-body">
               <h5 class="card-title"><b>Recent Posts</b><br></h5>
               <div class="list-group">
-                <a href="#" class="list-group-item list-group-item-action flex-column align-items-start">
+			  <?php 
+			     $recent = $pc->getAll();
+				 $count = 4;
+				 foreach($recent as $p){
+					if(!$count) break;
+				    $count--;
+				    if($p->getIdPost() != $postId){
+			  ?>
+                <a href=<?php echo '?postId='.$p->getIdPost(); ?> class="list-group-item list-group-item-action flex-column align-items-start">
                   <div class="d-flex w-100 justify-content-between datePost">
-                    <h5 class="mb-2 postTitle">Top 10 Things You Can Do In Summer!</h5>
+                    <h5 class="mb-2 postTitle"><?php echo $p->getTitle(); ?></h5>
                   </div>
                   <div class="row">
                     <div class="col-md-4"><img class="img-fluid d-block" src="https://static.pingendo.com/img-placeholder-1.svg"></div>
                     <div class="col-md-8">
-                      <p class="recentPost">Paragraph. Then, my friend, when darkness overspreads my eyes, and heaven and earth seem to dwell in my soul and absorb its power, like the form of a beloved mistress, then I often think with longing.</p>
-                    </div>
-                  </div>
-                </a><a href="#" class="list-group-item list-group-item-action flex-column align-items-start">
-                  <div class="d-flex w-100 justify-content-between datePost">
-                    <h5 class="mb-2 postTitle">Let's Read Some Books Together!</h5>
-                  </div>
-                  <div class="row">
-                    <div class="col-md-4"><img class="img-fluid d-block" src="https://static.pingendo.com/img-placeholder-1.svg"></div>
-                    <div class="col-md-8">
-                      <p class="recentPost">Paragraph. Then, my friend, when darkness overspreads my eyes, and heaven and earth seem to dwell in my soul and absorb its power, like the form of a beloved mistress, then I often think with longing.</p>
-                    </div>
-                  </div>
-                </a><a href="#" class="list-group-item list-group-item-action flex-column align-items-start">
-                  <div class="d-flex w-100 justify-content-between datePost">
-                    <h5 class="mb-2 postTitle">Let's Watch Movies Together!</h5>
-                  </div>
-                  <div class="row">
-                    <div class="col-md-4"><img class="img-fluid d-block" src="https://static.pingendo.com/img-placeholder-1.svg"></div>
-                    <div class="col-md-8">
-                      <p class="recentPost">Paragraph. Then, my friend, when darkness overspreads my eyes, and heaven and earth seem to dwell in my soul and absorb its power, like the form of a beloved mistress, then I often think with longing.</p>
+                      <p class="recentPost"><?php 
+					  $textArray = explode(" ", $p->getText());
+					  echo implode(" ",array_slice($textArray, 0, 10)).'...'; ?></p>
                     </div>
                   </div>
                 </a>
+				 <?php } } ?>
               </div>
             </div>
         </div>
